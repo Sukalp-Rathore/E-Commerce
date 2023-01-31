cdbe120258369a5b3586b6a226caab5bc0277d3b
Sukalp-Rathore <sukalprathore@gmail.com>
2023-02-01 01:26:42 +0530

    first comit

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+height:30px;
+background-color: teal;
+color: white ;
+display:flex;
+align-items:center;
+justify-content :center;
+font-size:14px;
+font-width:500;
+
+
+`
+
+const Announcement = () => {
+  return (
+    <Container>
+        Super deal! Free Shipping On Orders Over $50
+    </Container>
+  )
+}
+
+export default Announcement
